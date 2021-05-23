905492cc0e3de39ffe57cc7e5f4f3510ceaedac3
dr4z4 <dr4z4321@gmail.com>
2021-05-23 23:19:23 +0200

    homework resolved (without bonuses)

---
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/VizitkaController.java
@@ -0,0 +1,70 @@
+package cz.czechitas.java2webapps.ukol7.controller;
+
+import cz.czechitas.java2webapps.ukol7.entity.Vizitka;
+import cz.czechitas.java2webapps.ukol7.repository.VizitkaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+public class VizitkaController {
+
+    private final VizitkaRepository repository;
+
+    @Autowired
+    public VizitkaController(VizitkaRepository repository) {
+        this.repository = repository;
+    }
+
+    public void nullStringBinding(WebDataBinder binder) {
+        //prázdné textové řetězce nahradit null hodnotou
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public Object vizitky() {
+        return new ModelAndView("seznam")
+                .addObject("vizitky", repository.findAll());
+    }
+
+    @GetMapping("/{id:[0-9]+}")
+    public Object detail(@PathVariable Integer id) {
+        Optional<Vizitka> vizitka = repository.findById(id);
+        if (vizitka.isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+        return new ModelAndView("vizitka")
+                .addObject("vizitka", vizitka.get());
+    }
+
+    @GetMapping("/nova")
+    public Object nova() {
+        return new ModelAndView("formular")
+                .addObject("vizitka", new Vizitka());
+    }
+
+    @PostMapping("/nova")
+    public Object pridat(@ModelAttribute("vizitka") @Valid Vizitka vizitka, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "formular";
+        }
+        vizitka.setId(null);
+        repository.save(vizitka);
+        return "redirect:/";
+    }
+
+
+
+
+}
